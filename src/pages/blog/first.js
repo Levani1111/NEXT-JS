49050d2b1f5bc0c4d57173c30e6acd0a1d010af6
Levani1111 <papashvililevani@gmail.com>
2023-04-03 15:35:35 +0100

    init

---
+++ src/pages/blog/first.js
@@ -0,0 +1,5 @@
+function firstBlog() {
+  return <h1>First Blog Post</h1>;
+}
+
+export default firstBlog;
---
+++ src/pages/blog/secend.js
@@ -0,0 +1,9 @@
+function secendBlog() {
+  return (
+    <div>
+      <h1>Secend Blog</h1>
+    </div>
+  );
+}
+
+export default secendBlog;
