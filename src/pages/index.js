3dcfba8a70a65d8fe7bf937dbf84978c6bc257ce
Levani1111 <papashvililevani@gmail.com>
2023-04-10 13:24:59 +0100

    Navigating Proogrammatically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+
+  const handlerClick = () => { 
+    console.log("Place Order");
+    router.push("/product");
+  };
   return (
     <div>
       <h1>Home Page</h1>
       <Link href="/blog">Blog</Link>
       <br />
       <Link href="/product">Product</Link>
+      <button onClick={handlerClick}>
+        Place Order
+      </button>
     </div>
   );
 }
